e8ab3d1c93bcbdd7a3500f4b4bc232e35eed5d42
Julian Berle <berlejulian@gmail.com>
2023-01-19 21:58:48 +0100

    Added second game, added function in script.js for second game, changed a bit styling

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-const popupDiv = document.querySelector(".popup");
+const popupDiv1 = document.querySelector(".popup.spel1");
+const popupDiv2 = document.querySelector(".popup.spel2");
 const flexContainer = document.querySelector(".home .flex-container");
-const gameDiv = document.querySelector(".home .popup-content");
-const speluitlegDiv = document.querySelector(".home .speluitleg");
+const gameDiv1 = document.querySelector(".popup.spel1 .popup-content");
+const gameDiv2 = document.querySelector(".popup.spel2 .popup-content");
+const speluitlegDiv1 = document.querySelector(".popup.spel1 .speluitleg");
+const speluitlegDiv2 = document.querySelector(".popup.spel2 .speluitleg");
 
 function openPopUp() {
-    popupDiv.style.display = "block";
+    popupDiv1.style.display = "block";
+    flexContainer.style.display = "none";
+}
+
+function openPopUp2() {
+    popupDiv2.style.display = "block";
     flexContainer.style.display = "none";
 }
 
 function closePopUp() {
-    popupDiv.style.display = "none";
+    popupDiv1.style.display = "none";
+    popupDiv2.style.display = "none";
     flexContainer.style.display = "flex";
 }
 
 function startGame() {
-    speluitlegDiv.style.display = "none";
-    gameDiv.style.display = "block";
+    speluitlegDiv1.style.display = "none";
+    gameDiv1.style.display = "block";
+}
+
+function startGame2() {
+    speluitlegDiv2.style.display = "none";
+    gameDiv2.style.display = "block";
 }
